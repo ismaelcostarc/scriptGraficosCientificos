94b038a7cfb007849f5cd019b19b802dc543bee6
Ismael Assunção Costa <maelcosta96@gmail.com>
2021-03-16 22:26:11 -0300

    Ultimo commit

--- script.R
+++ script.R
@@ -5,14 +5,18 @@
 ###########################################################
 
 blocking <- "PBC"
+#blocking <- "PBC"
+#topology <- "EON"
 topology <- "NSFNET"
-xt <- "AXT"
+#xt <- "AXT"
+#xt <- "BXT"
+xt <- "AXTEXTRA"
 
 #Diretório e nome dos arquivos .csv e .pdf
 name <- paste(xt, topology, paste(blocking, topology, xt, sep=''), sep='/')
 data <- read.csv(paste("CSV/", name, ".csv", sep=''))
 # 6, 6 são largura e altura, respectivamente
-cairo_pdf(paste("Graficos/", name, ".pdf", sep=''), 6, 6, bg="transparent")
+cairo_pdf(paste("Graficos/", name, ".pdf", sep=''), 6.5, 6, bg="transparent")
 
 ###########################################################
 #
@@ -30,12 +34,14 @@ increment <- 100
 yintervals <- c(10E-7, 1)
 
 if(blocking == "PBB") {
-  ylabel <- "Probabilidade de bloqueio de banda"
+  ylabel <- "Probabilidade de
+  bloqueio de banda"
 } else {
-  ylabel <- "Probabilidade de bloqueio de circuito"
+  ylabel <- "Probabilidade de
+  bloqueio de circuito"
 }
 
-xlabel <- "Carga(Erlangs)"
+xlabel <- "Carga (Erlangs)"
 #Tamanho da borda da reta do intervalo de confiança
 intervalSize <- 0.07
 #Posição da legenda (left, right, top, bottom, bottomright, bottomleft...)
@@ -52,26 +58,8 @@ errorConfInt=TRUE
 #valores dos pontos de carga
 loads <- seq(initialLoad,initialLoad+increment*(qLoads-1),increment)
 
-## Para curvas em que o limite inferior do intervalo de confiança ultrapasse
-## o valor mínimo da curva, é necessário calcular o valor mínimo de cada curva
-## para corrigir um bug (o intervalo de confiança desaparece nos pontos em que
-## isso ocorre)
-#Encontra o valor mínimo do PGNIE
-# minpgnie <- 1
-# for(i in 1:qLoads) {
-#   if(data$PGNIE[i] < minpgnie && data$PGNIE[i] != 0)
-#     minpgnie <- data$PGNIE[i]
-# }
-# 
-# #Encontra o valor mínimo do ABNE
-# minabne <- 1
-# for(i in 1:qLoads) {
-#   if(data$ABNE[i] < minabne && data$ABNE[i] != 0)
-#     minabne <- data$ABNE[i]
-# }
-
 #Margens do grafico
-par(mar = c(5.5, 5.5, 1.3, 0.4))
+par(mar = c(6.7, 8.5, 0.4, 0.4))
 plot(data$PGNIE,
      type="l",
      xaxt="n",
@@ -80,21 +68,65 @@ plot(data$PGNIE,
      lty=1,
      ylim=yintervals,
      col="blue",
-     cex.lab=1.5,
      xlab="",
      ylab="")
 
-legend(legendPosition,
-       legend=c("PGNIE", "ABNE", "CP-RF", "RC-FF"),
-       lty=c(1,1,1,1), 
-       col=c("blue", "red", "green", "purple"),
-       bty="n",
-       #horiz=TRUE,
-       cex=1.2)
+if(xt == "BXT") {
+  #Legenda para os gráficos com Baixo XT
+  legend("topleft",
+         legend=c(
+           "PGNIE",
+           "ABNE"
+         ),
+         lty=c(1,1,1,1),
+         col=c("blue", "red"),
+         bty="n",
+         #horiz=TRUE,
+         cex=1.5)
+  
+  legend("topright",
+         legend=c(
+           "CP-RF",
+           "RC-FF"
+         ),
+         lty=c(1,1,1,1),
+         col=c("green", "purple"),
+         bty="n",
+         #horiz=TRUE,
+         cex=1.5)
+} else {
+  if(xt == "AXT") {
+    #Legenda para os gráficos com Alto XT
+    legend(legendPosition,
+           legend=c(
+             "PGNIE",
+             #"PGNIE-FFLF",
+             #"PGNIE-RFRF"
+             "ABNE",
+             "CP-RF",
+             "RC-FF"
+           ),
+           lty=c(1,1,1,1),
+           col=c("blue", "red", "green", "purple"),
+           bty="n",
+           #horiz=TRUE,
+           cex=1.5)
+  }
+  
+  if(xt == "AXTEXTRA") {
+    #Legenda para os gráficos com Alto XT Extra
+    legend(legendPosition,
+           legend=c("PGNIE-FFLF", "PGNIE-RFRF"),
+           lty=c(1,1),
+           col=c("blue", "red"),
+           bty="n",
+           cex=1.5)
+  }
+}
 
 #Rótulos dos eixos
-title(ylab=ylabel, mgp=c(4,1,0),cex.lab=1.5)
-title(xlab=xlabel, mgp=c(4.2,1,0),cex.lab=1.5)
+title(ylab=ylabel, mgp=c(4.5,0,0),cex.lab=2)
+title(xlab=xlabel, mgp=c(5.5,0,0),cex.lab=2)
 
 #Intervalo de confiança PGNIE
 for(i in 1:qLoads) {
@@ -188,98 +220,101 @@ for(i in 1:qLoads) {
   )
 }
 
-par(new=TRUE)
-plot(data$CPRF,
-     type="l",
-     axes=FALSE,
-     ann=FALSE,
-     log="y",
-     ylim=yintervals,
-     col="green"
-)
-
-#Intervalo de confiança CP-RF
-for(i in 1:qLoads) {
-  #Linha Vertical
-  segments(
-    i,
-    data$CPRF[i] + data$ERROCPRF[i],
-    i,
-    data$CPRF[i] - data$ERROCPRF[i],
-    col="black"
+if(xt != "AXTEXTRA") {
+  par(new=TRUE)
+  plot(data$CPRF,
+       type="l",
+       axes=FALSE,
+       ann=FALSE,
+       log="y",
+       ylim=yintervals,
+       col="green"
   )
   
-  #Linha Horizontal Superior
-  segments(
-    i-intervalSize,
-    data$CPRF[i] + data$ERROCPRF[i],
-    i+intervalSize,
-    data$CPRF[i] + data$ERROCPRF[i],
-    col="black"
+  #Intervalo de confiança CP-RF
+  for(i in 1:qLoads) {
+    #Linha Vertical
+    segments(
+      i,
+      data$CPRF[i] + data$ERROCPRF[i],
+      i,
+      data$CPRF[i] - data$ERROCPRF[i],
+      col="black"
+    )
+    
+    #Linha Horizontal Superior
+    segments(
+      i-intervalSize,
+      data$CPRF[i] + data$ERROCPRF[i],
+      i+intervalSize,
+      data$CPRF[i] + data$ERROCPRF[i],
+      col="black"
+    )
+    
+    #Linha Horizontal Inferior
+    segments(
+      i-intervalSize,
+      data$CPRF[i] - data$ERROCPRF[i],
+      i+intervalSize,
+      data$CPRF[i] - data$ERROCPRF[i],
+      col="black"
+    )
+  }
+  
+  par(new=TRUE)
+  plot(data$RCFF,
+       type="l",
+       axes=FALSE,
+       ann=FALSE,
+       log="y",
+       ylim=yintervals,
+       col="purple"
   )
   
-  #Linha Horizontal Inferior
-  segments(
-    i-intervalSize,
-    data$CPRF[i] - data$ERROCPRF[i],
-    i+intervalSize,
-    data$CPRF[i] - data$ERROCPRF[i],
-    col="black"
-  )
-}
-
-par(new=TRUE)
-plot(data$RCFF,
-     type="l",
-     axes=FALSE,
-     ann=FALSE,
-     log="y",
-     ylim=yintervals,
-     col="purple"
-)
-
-#Intervalo de confiança RC-FF
-for(i in 1:qLoads) {
-  #Linha Vertical
-  segments(
-    i,
-    data$RCFF[i] + data$ERRORCFF[i],
-    i,
-    data$RCFF[i] - data$ERRORCFF[i],
-    col="black"
-  )
+  #Intervalo de confiança RC-FF
+  for(i in 1:qLoads) {
+    #Linha Vertical
+    segments(
+      i,
+      data$RCFF[i] + data$ERRORCFF[i],
+      i,
+      data$RCFF[i] - data$ERRORCFF[i],
+      col="black"
+    )
+    
+    #Linha Horizontal Superior
+    segments(
+      i-intervalSize,
+      data$RCFF[i] + data$ERRORCFF[i],
+      i+intervalSize,
+      data$RCFF[i] + data$ERRORCFF[i],
+      col="black"
+    )
+    
+    #Linha Horizontal Inferior
+    segments(
+      i-intervalSize,
+      data$RCFF[i] - data$ERRORCFF[i],
+      i+intervalSize,
+      data$RCFF[i] - data$ERRORCFF[i],
+      col="black"
+    )
+  }
   
-  #Linha Horizontal Superior
-  segments(
-    i-intervalSize,
-    data$RCFF[i] + data$ERRORCFF[i],
-    i+intervalSize,
-    data$RCFF[i] + data$ERRORCFF[i],
-    col="black"
-  )
-  
-  #Linha Horizontal Inferior
-  segments(
-    i-intervalSize,
-    data$RCFF[i] - data$ERRORCFF[i],
-    i+intervalSize,
-    data$RCFF[i] - data$ERRORCFF[i],
-    col="black"
-  )
 }
 
 #Valores do eixo x
 axis(side = 1,
      at = 1:qLoads, 
      label = loads,
-     cex.axis=1.5,
+     cex.axis=2,
      las=2)
 
 #Valores do eixo x
 axis(side = 2,
      at=c(1E-6, 1E-5, 1E-4, 1E-3, 1E-2, 1E-1, 1),
-     labels=c("10⁻⁷", "10⁻⁶", "10⁻⁵","10⁻⁴","10⁻³", "10⁻²", "10⁻¹"),
+     labels=c("10⁻⁶", "10⁻⁵","10⁻⁴","10⁻³", "10⁻²", "10⁻¹", "10⁰  "),
      las=2,
-     cex.axis=1.5)
+     cex.axis=2)
 
 dev.off()
